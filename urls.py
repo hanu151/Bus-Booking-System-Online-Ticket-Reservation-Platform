32824714a2b40943d94c64561bfa5311f1fe7f5f
hanu151 <hkoli0206@gmail.com>
2026-06-01 15:57:28 +0530

    tavels

--- urls.py
+++ urls.py
@@ -1,12 +1,11 @@
-
 from django.urls import path
-from .views import RegisterView, LoginView, BusListCreateView, UserBookingView, BookingView, BusDetailView
+from .views import RegisterView, LoginView, BusListCreateView, BusDetailView, BookingView, UserBookingView
 
 urlpatterns = [
-    path('buses/', BusListCreateView.as_view(), name='buslist'),
+    path('register/', RegisterView.as_view(), name='register'),
+    path('login/', LoginView.as_view(), name='login'),
+    path('buses/', BusListCreateView.as_view(), name='bus-list'),
     path('buses/<int:pk>/', BusDetailView.as_view(), name='bus-detail'),
-    path('register/', RegisterView.as_view(), name = 'register'),
-    path('login/', LoginView.as_view(), name = 'login'),
-    path('user/<int:user_id>/bookings/', UserBookingView.as_view(), name="user-bookings"),
-    path('booking/', BookingView.as_view(), name="booking")
+    path('book/', BookingView.as_view(), name='book-seat'),
+    path('my-bookings/<int:user_id>/', UserBookingView.as_view(), name='user-bookings'),
 ]
